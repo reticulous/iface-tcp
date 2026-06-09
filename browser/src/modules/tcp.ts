fd7ad257e584eed00d700096a5c9479027b6ad20
Rop Gonggrijp <rop@gonggri.jp>
2026-06-09 15:07:51 +0000

    browser: move TCP iface menu to settings/mesh/interfaces
    Signed-off-by: Rop Gonggrijp <rop@gonggri.jp>

--- browser/src/modules/tcp.ts
+++ browser/src/modules/tcp.ts
@@ -2,5 +2,7 @@ import { useMenuStore } from 'spangap-browser/stores/menu'
 import TcpPanel from '../panels/TcpPanel.vue'
 
 export function registerTcp() {
-  useMenuStore().register('settings/reticulum/transports/tcp', 'TCP', { type: 'panel', component: TcpPanel })
+  const menu = useMenuStore()
+  menu.setMenu('settings/mesh/interfaces', { label: 'RNS Interfaces', placement: 2 })
+  menu.register('settings/mesh/interfaces/tcp', 'TCP', { type: 'panel', component: TcpPanel })
 }
